90151d4b89f4fe73a9c1a503c47082b0bcc8829d
Sulav Shrestha <sulavshrestha@Sulavs-MacBook-Air.local>
2024-11-23 16:31:07 -0500

    changed cors to accept request from everything

--- server/server.mjs
+++ server/server.mjs
@@ -10,9 +10,7 @@ const app = express()
 const PORT = process.env.PORT ?? 3000
 
 // Middleware
-app.use(cors({
-  origin: '*',
-}));
+app.use(cors());
 app.use(express.json());
 
 // Auth middleware
